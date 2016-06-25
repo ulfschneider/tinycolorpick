062dc7bfe70d9b2aedd0be38ed061e1c583ae025
ulfschneider <u@ulf.codes>
2016-06-25 13:31:51 +0200

    Add link to github

--- package.js
+++ package.js
@@ -1,15 +1,14 @@
 Package.describe({
-  name: "ulfeed:tinycolorpick",
+  name: "ulfschneider:tinycolorpick",
   version: "1.1.0",
   summary: "An adapted version of http://rachel-carvalho.github.io/simple-color-picker/",
-  git: "",
+  git: "https://github.com/ulfschneider/tinycolorpick",
   documentation: "README.md"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("1.3.2.4");
   api.use(["ecmascript", "templating", "jquery"]);
-  api.mainModule("tinyColorPick.js", "client");
 	
   api.addFiles([
      "simple-color-picker.js",
@@ -17,14 +16,14 @@ Package.onUse(function(api) {
      "tinyColorPick.html",
      "tinyColorPick.js"
    ], ["client"]);
-
+	api.export("tinyColorPick", "client");
 });
 
 
 Package.onTest(function(api) {
   api.use(["ecmascript", "templating", "jquery"]);
   api.use("tinytest");
-  api.use("ulfeed:tinycolorpick");
+  api.use("ulfschneider:tinycolorpick");
   api.mainModule("tinyColorPick-tests.js");
 });
 
